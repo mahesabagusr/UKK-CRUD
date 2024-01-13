77d04ec08a3a30d1d1e07b6c70435824d197394f
mahesabagusr <mahesabagusraditya1@gmail.com>
2024-01-13 15:16:16 +0700

    fix cors2

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,18 @@ config({ path: '.env' })
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // or '*' for any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
 app.use(cookieParser())
 app.use(express.json());
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
-
 app.use(router);
 
 sequelize.authenticate()
